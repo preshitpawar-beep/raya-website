f131b8e27f986004ee91e6af8e77218408a7c81d
preshitpawar-beep <preshitpawar@gmail.com>
2026-03-11 21:35:14 +0000

    Delete src directory

--- src/app/robots.js
+++
@@ -1,9 +0,0 @@
-// src/app/robots.js
-import { SITE } from '@/lib/seo';
-
-export default function robots() {
-  return {
-    rules: { userAgent: '*', allow: '/', disallow: '' },
-    sitemap: `${SITE.url}/sitemap.xml`,
-  };
-}
